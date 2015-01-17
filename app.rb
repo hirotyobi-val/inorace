a9dc32f43764cd7c4b7b4ee81dc605cea93a0af4
hirotyobi <hirotyobi@hirotyobi-no-MacBook-Pro.local>
2015-01-17 21:24:27 +0900

    走者数の指定が可能に
    例:10頭立ての場合、localhost:9292/race?runners=10

--- app.rb
+++ app.rb
@@ -10,4 +10,14 @@ class MyApp < Sinatra::Base
     @race.run
     erb :debug_race_info
   end
+
+  get '/race' do
+    runners = 5
+    runners = params['runners'].to_i if @params['runners'].to_i > 1
+
+    @race = Race.new
+    @race.set_normal_race(Course.new(1000), runners)
+    @race.run
+    erb :debug_race_info
+  end
 end
