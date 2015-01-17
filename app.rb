f7dad6d1c393ed5d375a0ddf436a7d17745e2fc4
hirotyobi <hirotyobi@hirotyobi-no-MacBook-Pro.local>
2015-01-18 04:37:08 +0900

    [WIP]外部からパラメータを与えられるためにするための途中まで

--- app.rb
+++ app.rb
@@ -20,4 +20,15 @@ class MyApp < Sinatra::Base
     @race.run
     erb :debug_race_info
   end
+
+  # TODO : 外からパラメータで具体的な値を指定できるようにする
+  get '/original' do
+    runners = 5
+    runners = params['runners'].to_i if @params['runners'].to_i > 1
+
+    @race = Race.new
+    @race.set_original_race(Course.new(64000), runners, [798, 799, 800, 801, 802], [60, 48, 36, 24, 12])
+    @race.run
+    erb :debug_race_info    
+  end
 end
